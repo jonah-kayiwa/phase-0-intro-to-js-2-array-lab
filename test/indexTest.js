07d168e63ce24695dc29de9fe11b47f9b879bb26
jonah-kayiwa <kayiwa.jonah@student.moringaschool.com>
2024-01-03 11:44:08 +0300

    ##c##

--- test/indexTest.js
+++ test/indexTest.js
@@ -21,6 +21,7 @@ describe('index.js', function () {
     });
 
     describe('destructivelyPrependCat(name)', function () {
+      
       it('prepends a cat to the beginning of the cats array', function () {
         destructivelyPrependCat("Bob");
 
